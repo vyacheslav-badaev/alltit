64ab1194a17761897c9a9543c77279d29ae9d90b
Vyacheslav <vyacheslav.badaev@devit.group>
2021-12-23 15:41:25 +0000

    feat: testing ci update

--- test/Logo.spec.js
+++ test/Logo.spec.js
@@ -3,6 +3,6 @@ import Logo from '@/components/Logo.vue'
 describe('Logo', () => {
   test('is a Vue instance', () => {
     const wrapper = mount(Logo)
-    expect(wrapper.isVueInstance()).toBe('asdsa')
+    expect(wrapper.isVueInstance()).toBeTruthy()
   })
 })
