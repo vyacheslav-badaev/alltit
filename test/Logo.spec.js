29a80e729ccc923e610e021329f82eeea3854a58
Vyacheslav <vyacheslav.badaev@devit.group>
2022-05-27 16:15:57 +0000

    updates

--- test/Logo.spec.js
+++ test/Logo.spec.js
@@ -3,6 +3,6 @@ import Logo from '@/components/Logo.vue'
 describe('Logo', () => {
   test('is a Vue instances s', () => {
     const wrapper = mount(Logo)
-    expect(wrapper.isVueInstance()).toBe('asda')
+    expect(wrapper.isVueInstance()).toBeTruthy()
   })
 })
